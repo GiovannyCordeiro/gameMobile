e1793fc512e9d62b27e8a851b1619dae2bacada5
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2024-01-09 11:51:30 -0300

    more styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,15 +4,27 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return(
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad"/>
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput 
+        style={styles.numberInput} 
+        maxLength={2} 
+        keyboardType="number-pad"
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
     marginTop: 100,
     marginHorizontal: 24,
@@ -34,5 +46,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row'
+  },
+  buttonContainer: {
+    flex: 1
   }
 })
