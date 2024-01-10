5e85b5f04ac21906dc973601c33fc423a7be8339
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2024-01-10 15:07:59 -0300

    feat: adding processing for inputs

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,6 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native';
 
-export default function PrimaryButton({children}) {
-
-  function pressHandler(){
-    console.log("presed");
-  }
+export default function PrimaryButton({children, onPress}) {
 
   return(
     <View style={styles.buttonOuterContainer}>
@@ -14,7 +10,7 @@ export default function PrimaryButton({children}) {
           ? [styles.buttonInnerContainer, styles.pressed] 
           : styles.buttonInnerContainer
         }
-        onPress={pressHandler} 
+        onPress={onPress} 
         android_ripple={{color: '#640233'}} 
       >
         <Text style={styles.buttonText}>{children}</Text>
